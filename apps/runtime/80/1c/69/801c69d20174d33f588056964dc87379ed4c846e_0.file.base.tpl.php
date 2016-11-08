b1f591655a12f35c582a14c0b3a9a43c1034572a
ed <ed.proff@gmail.com>
2016-11-08 11:37:28 +0300

    update

--- apps/runtime/80/1c/69/801c69d20174d33f588056964dc87379ed4c846e_0.file.base.tpl.php
+++ apps/runtime/80/1c/69/801c69d20174d33f588056964dc87379ed4c846e_0.file.base.tpl.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 3.1.31-dev/40, created on 2016-11-06 19:38:21
+/* Smarty version 3.1.31-dev/40, created on 2016-11-07 16:25:01
   from "/Users/edik/web/fastest.dev/apps/app/views/frontend/smarty/base.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.31-dev/40',
-  'unifunc' => 'content_581f86addee675_26967429',
+  'unifunc' => 'content_5820aadd626218_13787675',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_581f86addee675_26967429 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5820aadd626218_13787675 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html lang="en"><head><meta charset="UTF-8"><title><?php echo $_smarty_tpl->tpl_vars['app']->value['title'];?>
