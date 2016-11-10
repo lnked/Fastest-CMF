9718dd1f491551d086fe398233d4e628ddce905c
ed <ed.proff@gmail.com>
2016-11-10 05:22:04 +0300

    update

--- public_html/cp/frontend/scripts/app/_app.history.js
+++ public_html/cp/frontend/scripts/app/_app.history.js
@@ -51,26 +51,24 @@ var app = app || {};
             //   }
             // });
 
-            History = window.history;
-
-            console.log(window.History, History, History.enabled);
+            console.log(window.History);
 
             window.history.pushState({}, null, '/cp/site/edik');
 
-            // // Bind to StateChange Event
-            History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
-                var State = History.getState(); // Note: We are using History.getState() instead of event.state
-            });
+            // Bind to StateChange Event
+            // History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+            //     var State = History.getState(); // Note: We are using History.getState() instead of event.state
+            // });
 
-            // // Change our States
-            History.pushState({state:1}, "State 1", "?state=1"); // logs {state:1}, "State 1", "?state=1"
-            History.pushState({state:2}, "State 2", "?state=2"); // logs {state:2}, "State 2", "?state=2"
-            History.replaceState({state:3}, "State 3", "?state=3"); // logs {state:3}, "State 3", "?state=3"
-            History.pushState(null, null, "?state=4"); // logs {}, '', "?state=4"
-            History.back(); // logs {state:3}, "State 3", "?state=3"
-            History.back(); // logs {state:1}, "State 1", "?state=1"
-            History.back(); // logs {}, "Home Page", "?"
-            History.go(2); // logs {state:3}, "State 3", "?state=3"
+            // // // Change our States
+            // History.pushState({state:1}, "State 1", "?state=1"); // logs {state:1}, "State 1", "?state=1"
+            // History.pushState({state:2}, "State 2", "?state=2"); // logs {state:2}, "State 2", "?state=2"
+            // History.replaceState({state:3}, "State 3", "?state=3"); // logs {state:3}, "State 3", "?state=3"
+            // History.pushState(null, null, "?state=4"); // logs {}, '', "?state=4"
+            // History.back(); // logs {state:3}, "State 3", "?state=3"
+            // History.back(); // logs {state:1}, "State 1", "?state=1"
+            // History.back(); // logs {}, "Home Page", "?"
+            // History.go(2); // logs {state:3}, "State 3", "?state=3"
             
             /*  Инициализируем контейнер для записей */
             var $entries = $(".wrapper__content");
@@ -94,13 +92,13 @@ var app = app || {};
                 с дополнительными данными, которые можно извлечь в обработчике
                 изменения состояния, описанном ниже.
                 В нашем случае это будет пустой объект. */
-                History.pushState({}, null, url);
+                window.history.pushState({}, null, url);
             });
 
             /*  Готовим обработчик изменения состояния страницы */
-            History.Adapter.bind(window, "statechange", function () {
+            window.history.Adapter.bind(window, "statechange", function () {
                 /*  Получаем информацию о состоянии страницы */
-                var state = History.getState();
+                var state = window.history.getState();
 
                 /* Получаем URL нового состояния. Это URL, который мы передали
                 в .pushState() */
