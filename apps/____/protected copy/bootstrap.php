45fa2baae4a660528e84dfe51a4f975c2da1749c
ed <ed.proff@gmail.com>
2016-11-12 19:47:54 +0300

    update

--- apps/____/protected copy/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/dispatch.php';
-require __DIR__ . '/functions.php';
-require __DIR__ . '/singleton.php';
-require __DIR__ . '/controllers/product.php';
-require __DIR__ . '/router.php';
-
-require __DIR__ . '/app.php';
